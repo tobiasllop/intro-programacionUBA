2ab53505b548f26cdbad027bf74428003a21ac16
Tobias Llop <tobiasllop@gmail.com>
2023-04-27 12:56:07 -0300

    Add files via upload

--- Guia5.hs
+++ Guia5.hs
@@ -16,78 +16,82 @@ reverso [] = []
 reverso (x:xs) = reverso xs ++ [x]
 
 --2
+pertenece :: (Eq t) => t -> [t] -> Bool
+pertenece x xs = elem x xs
+
+todosIguales :: (Eq t) => [t] -> Bool
+todosIguales [x] = True
+todosIguales (x:xs) | pertenece x xs = todosIguales xs
+                    | otherwise = False
+
 todosDistintos :: (Eq t) => [t] -> Bool
-todosDistintos [] = True
+todosDistintos [x] = True
 todosDistintos (x:xs) | elem x xs = False
                       | otherwise = todosDistintos xs
 
+
+hayRepetidos :: (Eq t) => [t] -> Bool
+hayRepetidos [x] = False
+hayRepetidos (x : xs) | pertenece x xs = True
+                      | otherwise = hayRepetidos xs
+
+quitar :: (Eq t) => t -> [t] -> [t]
+quitar _ [] = []
+quitar x (y : ys) | y == x = ys
+                  | otherwise = y : quitar x ys
+
+quitarTodos :: (Eq t ) => t -> [t] -> [t]
+quitarTodos _[] = []
+quitarTodos n m | pertenece n m = quitarTodos n (quitar n m)
+                | otherwise = m
+
+eliminarRepetidos :: (Eq t) => [t] -> [t]
+eliminarRepetidos [] = []
+eliminarRepetidos [x] = [x]
+eliminarRepetidos (x:xs) = [x] ++ (eliminarRepetidos (quitarTodos x xs))  
+
+mismosElementos :: (Eq t) => [t] -> [t] -> Bool
+mismosElementos [] [] = True 
+mismosElementos (x:xs) l = pertenece x l == mismosElementos (quitarTodos x (eliminarRepetidos xs)) (quitarTodos x (eliminarRepetidos l)) 
+
+capicua :: (Eq t) => [t] -> Bool
+capicua l = l == reverso l 
+
 --3
+sumatoria :: [Integer] -> Integer
+sumatoria [] = 0
+sumatoria (x : xs) = x + sumatoria xs
+
+productoria :: [Integer] -> Integer
+productoria [] = 1
+productoria (x:xs) = x * productoria xs
+
 maximo :: [Integer] -> Integer
 maximo [x] = x
 maximo (x:xs) | (head xs) >= x = maximo xs 
               | otherwise = x
 
+sumarN :: Integer -> [Integer] -> [Integer]
+sumarN n [] = []
+sumarN n (x : xs) = (x + n) : sumarN n xs
 
-{-
+sumarElPrimero :: [Integer] -> [Integer]
+sumarElPrimero (x : xs) = x + x : sumarN x xs
 
+sumarElUltimo :: [Integer] -> [Integer]
+sumarElUltimo [] = []
+sumarElUltimo (x : xs) = x + last (x:xs) : sumarElUltimo xs
 
-principio :: [t] -> [t]
-principio (x:xs) | null xs == True = [] ++ []
-                 | otherwise = ([] ++ [x]) ++ principio xs
-reverso :: [t] -> [t]
-reverso (x:xs) | null xs == True = [x] ++ []
-               | otherwise = reverso xs ++ ([x] ++ []) 
-pertenece :: (Eq t) => t -> [t] -> Bool
-pertenece n t | n == head t = True
-              | null (tail t) == True && n /= head t = False
-              | otherwise = pertenece n (tail t)
-todosIguales :: (Eq t) => [t] -> Bool
-todosIguales [] = True
-todosIguales t | head t == (head (tail t)) && null (tail (tail t)) == True = True
-               | head t /= (head (tail t)) = False
-               | otherwise = todosIguales (tail t)
-subTodosDistintos :: (Eq t) => [t] -> [t] -> [t] -> Bool
-subTodosDistintos n m [] = True
-subTodosDistintos n m q | longitud n == 1 = True 
-                        | null (tail (tail n)) == True && head n /= head q = subTodosDistintos m (principio m) (ultimo m) 
-                        | head n == head q = False 
-                        | otherwise = subTodosDistintos (tail n) m q 
-todosDistintos :: (Eq t) => [t] -> Bool
-todosDistintos n = subTodosDistintos n (principio n) (ultimo n)
-subhayRepetidos :: (Eq t) => [t] -> [t] -> [t] -> Bool
-subhayRepetidos n m [] = False
-subhayRepetidos n m q | longitud n == 1 = False 
-                   | null (tail (tail n)) == True && head n /= head q = subhayRepetidos m (principio m) (ultimo m) 
-                   | head n == head q = True
-                   | otherwise = subhayRepetidos (tail n) m q 
-hayRepetidos :: (Eq t) => [t] -> Bool
-hayRepetidos n = subhayRepetidos n (principio n) (ultimo n)
-subquitar :: (Eq t) => t -> [t] -> [t] -> [t]
-subquitar n [] h = []
-subquitar n (x:xs) h | n == x = h ++ [] ++ xs
-                  | otherwise = subquitar n xs (h ++ [x])
-quitar :: (Eq t) => t -> [t] -> [t]
-quitar n t = subquitar n t []
-quitarTodos :: (Eq t ) => t -> [t] -> [t] 
-quitarTodos n m | pertenece n m == False = m 
-                | otherwise = quitarTodos n (quitar n m)
-eliminarRepetidos :: (Eq t) => [t] -> [t]
-eliminarRepetidos [] = []
-eliminarRepetidos (x:xs) | null xs == True = [] ++ [x] 
-                         | otherwise = [x] ++ (eliminarRepetidos (quitarTodos x xs))   
-submismosElementos :: (Eq t) => [t] -> [t] -> Bool
-submismosElementos [] _ = False
-submismosElementos _ [] = False
-submismosElementos (x:xs) t | pertenece x t == False = False
-                         | null xs == True && pertenece x t == True = True
-                         | otherwise = submismosElementos xs t
-mismosElementos :: (Eq t) => [t] -> [t] -> Bool
-mismosElementos n t | submismosElementos n t == True && submismosElementos t n == True = True
-                    | otherwise = False
---capicua :: (Eq t) => [t] -> Bool
-cortarDesdeHasta :: (Eq t) => [t] -> Int -> Int -> [t]
-cortarDesdeHasta n a b | b == 0 = [] ++ [head n]
-                      | a == 0 = cortarDesdeHasta (tail n) a (b-1) ++ [head n]
-                      | otherwise = cortarDesdeHasta (tail n) (a-1) b
-                      
--}
+pares :: [Integer] -> [Integer]
+pares [] = []
+pares (x : xs) | x `mod` 2 == 0 = x : pares xs
+               | otherwise = pares xs
+
+multiplosDeN :: Integer -> [Integer] -> [Integer]
+multiplosDeN n [] = []
+multiplosDeN n (x : xs) | x `mod` n == 0 = x : multiplosDeN n xs
+                        | otherwise = multiplosDeN n xs
+
+ordenar :: [Integer] -> [Integer]
+ordenar [] = []
+ordenar l = ordenar (quitar (maximo l) l) ++ [maximo l] 
